6b0197b80f1540db8a4971a2122d0c4c9d957af0
Paul Taylor <me@ptylr.com>
2021-04-15 09:15:33 +0100

    Refactored admin.js to prevent Gatsby creating the page-type.

---
+++ React/Procam-Gatsby/src/restricted/admin.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+import HeaderNavigationAdmin from "../components/admin/headerNavigationAdmin"
+
+const Admin = () => {
+  // CmsPage
+  return (
+    <HeaderNavigationAdmin/>
+  )
+}
+
+export default Admin;
